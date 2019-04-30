12114f2fe19d65a7e4cfedadf426da664305103f
bitegarden <bitegarden@gmail.com>
2019-04-30 13:51:27 +0200

    Add customer logos

--- js/main.js
+++ js/main.js
@@ -10,12 +10,9 @@ $(document).ready(function(){
 	    autoPlay: 3000,
 	});
 
-
-
-
 	$("#clients-logo").owlCarousel({
 		autoPlay: 3000,
-		items : 5,
+		items : 7,
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
 	});
